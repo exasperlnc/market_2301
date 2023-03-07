269204f7f0c70e188391953e52d310ae2b310e2f
Logan Cole <emtlogancole@gmail.com>
2023-03-07 10:53:10 -0600

    feat: market class

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,22 @@
+require_relative 'vendor'
 class Market
-  
+  attr_reader :name, :vendors
+  def initialize(name)
+    @name = name
+    @vendors = []
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map {|vendor| vendor.name}
+  end
+
+  def vendors_that_sell(item)
+    vendors_with_item = []
+    vendors.each {|vendor| vendors_with_item << vendor if vendor.check_stock(item)}
+    vendors_with_item
+  end
 end
